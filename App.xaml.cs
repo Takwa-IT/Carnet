a6c446b9ac9d40cb37bf87d90ec965e8213256df
Smeti Takwa <smatitakwapro@gmail.com>
2025-12-05 14:02:09 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using carnet.Models.Business;
+using carnet.services.business;
+namespace carnet
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var settings = SettingsService.Instance;
+
+
+            Task.Run(async () =>
+            {
+                await DatabaseService.InitializeAsync(); 
+                await NoteService.Instance.LoadNotesAsync(userId:0); 
+            }).Wait();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace carnet
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
